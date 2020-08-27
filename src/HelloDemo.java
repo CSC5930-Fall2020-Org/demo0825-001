ce800be6e19a9a4ef1cba68756dac99e4cfd4074
ms-O <kristin.obermyer@villanova.edu>
2020-08-27 11:27:00 -0400

    right msg

--- src/HelloDemo.java
+++ src/HelloDemo.java
@@ -2,10 +2,12 @@
 public class HelloDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        
-        
-        
+        if (args != null && args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        }
+        else {
+            System.out.println("No args specified");
+        }
 
     }
 
