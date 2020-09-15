6e00c89d922dcba9a2affd6a3529d837232cbf12
ms-O <kristin.obermyer@villanova.edu>
2020-09-15 15:45:56 -0400

    Refactoring demo

--- src/RefactoringDemoFBB.java
+++ src/RefactoringDemoFBB.java
@@ -1,11 +1,8 @@
-import java.util.*;
-import java.io.*;
-//import java.io.PrintWriter;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-//import java.io.IOException; 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 //Student code from a prior semester - used to demo Checkstyle
 public class RefactoringDemoFBB {
@@ -13,9 +10,13 @@ public class RefactoringDemoFBB {
     public static void main(String args[])
             throws FileNotFoundException, IOException {
 
+        RefactoringDemoFBB demoFbb = new RefactoringDemoFBB();
+        demoFbb.run(args);
+    }
+
+    protected void run(String[] args) throws FileNotFoundException {
         String count = "0";
         String argument = "";
-        double test = 0;
 
         if (args.length == 0) {
             argument = "console";
@@ -26,118 +27,122 @@ public class RefactoringDemoFBB {
         }
 
         if (argument == "batch") {
-            // Path filepath = Paths.get("\\src\\resources\\fbb-input.txt");
-            File filepath = new File(".\\src\\fbb-input.txt");
-            Scanner scanFile = new Scanner(filepath);
-            PrintWriter outFile = new PrintWriter("fbb-actual-output.txt");
-
-            while (scanFile.hasNext()) {
-                count = scanFile.nextLine();
-                count.trim();
-
-                try {
-                    test = Double.parseDouble(count);
-                }
-                catch (NumberFormatException e) {
-                    outFile.println(count + " = Invalid Input");
-                    continue;
-                }
-
-                if (test < 0) {
-                    outFile.println(count + " = Invalid Input");
-                    continue;
-                }
-
-                if (test > 2147483647) {
-                    outFile.println(count + " = Invalid Input");
-                    continue;
-                }
-
-                if (test % 1 != 0) {
-                    outFile.println(count + " = Invalid Input");
-                    continue;
-                }
-
-                if (test % 3 == 0) {
-                    outFile.print("Foo");
-                }
-
-                if (test % 5 == 0) {
-                    outFile.print("Bar");
-                }
-
-                if (test % 7 == 0) {
-                    outFile.print("Baz");
-                }
-
-                if (test % 7 != 0 && test % 5 != 0 && test % 3 != 0) {
-                    int printValue = (int) test;
-                    outFile.print(printValue);
-                }
-
-                outFile.println("");
-
-            } // End of While Loop
-            outFile.close();
+            runBatch();
         }
 
         if (argument == "console") {
-            Scanner scan = new Scanner(System.in);
-            String answer = "";
+            runConsole();
+        }
+    }
 
-            while (!answer.equals("Stop")) {
+    protected void runBatch() throws FileNotFoundException {
+        String count;
+        double test = 0;
+        // Path filepath = Paths.get("\\src\\resources\\fbb-input.txt");
+        File filepath = new File(".\\src\\fbb-input.txt");
+        Scanner scanFile = new Scanner(filepath);
+        PrintWriter outFile = new PrintWriter("fbb-actual-output.txt");
 
-                System.out.println("Please Enter a Value: ");
-                count = scan.nextLine();
-                count.trim();
+        while (scanFile.hasNext()) {
+            count = scanFile.nextLine();
+            count.trim();
 
-                try {
-                    test = Double.parseDouble(count);
-                }
-                catch (NumberFormatException e) {
-                    System.out.println(count + " = Invalid Input");
-                    continue;
-                }
+            try {
+                test = Double.parseDouble(count);
+            }
+            catch (NumberFormatException e) {
+                outFile.println(count + " = Invalid Input");
+                continue;
+            }
 
-                if (test < 0) {
-                    System.out.println(count + " = Invalid Input");
-                    continue;
-                }
+            if (test < 0) {
+                outFile.println(count + " = Invalid Input");
+                continue;
+            }
 
-                if (test > 2147483647) {
-                    System.out.println(count + " = Invalid Input");
-                    continue;
-                }
+            if (test > Integer.MAX_VALUE) {
+                outFile.println(count + " = Invalid Input");
+                continue;
+            }
 
-                if (test % 1 != 0) {
-                    System.out.println(count + " = Invalid Input");
-                    continue;
-                }
+            if (test % 1 != 0) {
+                outFile.println(count + " = Invalid Input");
+                continue;
+            }
 
-                if (test % 3 == 0) {
-                    System.out.print("Foo");
-                }
+            String transformedValue = transform(test);
+            outFile.println(transformedValue);
 
-                if (test % 5 == 0) {
-                    System.out.print("Bar");
-                }
+        } // End of While Loop
+        outFile.close();
+    }
 
-                if (test % 7 == 0) {
-                    System.out.print("Baz");
-                }
+    protected void runConsole() {
+        String count;
+        double userInput = 0;
+        Scanner scan = new Scanner(System.in);
+        String answer = "";
 
-                if (test % 7 != 0 && test % 5 != 0 && test % 3 != 0) {
-                    int printValue = (int) test;
-                    System.out.print(printValue);
-                }
+        while (!answer.equals("Stop")) {
 
-                System.out.println("");
-                System.out.println(
-                        "Would you like to End Program? (Type \"Stop\" to End) ");
-                answer = scan.nextLine();
+            System.out.println("Please Enter a Value: ");
+            count = scan.nextLine();
+            count.trim();
 
-            } // End of While Loop
+            try {
+                userInput = Double.parseDouble(count);
+            }
+            catch (NumberFormatException e) {
+                System.out.println(count + " = Invalid Input");
+                continue;
+            }
+
+            if (userInput < 0) {
+                System.out.println(count + " = Invalid Input");
+                continue;
+            }
+
+            if (userInput > Integer.MAX_VALUE) {
+                System.out.println(count + " = Invalid Input");
+                continue;
+            }
+
+            if (userInput % 1 != 0) {
+                System.out.println(count + " = Invalid Input");
+                continue;
+            }
+
+            String transformedValue = transform(userInput);
+            System.out.println(transformedValue);
+
+            System.out.println(
+                    "Would you like to End Program? (Type \"Stop\" to End) ");
+            answer = scan.nextLine();
+
+        } // End of While Loop
+    }
+
+    protected String transform(double userInput) {
+        StringBuilder buffer = new StringBuilder();
+
+        if (userInput % 3 == 0) {
+            buffer.append("Foo");
         }
 
+        if (userInput % 5 == 0) {
+            buffer.append("Bar");
+        }
+
+        if (userInput % 7 == 0) {
+            buffer.append("Baz");
+        }
+
+        if (userInput % 7 != 0 && userInput % 5 != 0 && userInput % 3 != 0) {
+            int printValue = (int) userInput;
+            buffer.append(printValue);
+        }
+
+        return buffer.toString();
     }
+
 }
